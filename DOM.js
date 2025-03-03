1f89c455a20a5479092a787acd7dcb259713748d
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-03-03 21:25:11 +0300

    update

--- DOM.js
+++ DOM.js
@@ -1,101 +1,85 @@
-//dom manipulation
-//querying the dom
-const para = document.querySelector('p');
-console.log(para);
+// Function to change text of multiple paragraphs
+const changeParagraphText = (elements, newText) => {
+    elements.forEach(para => {
+        if (para) {
+            para.innerText = newText;
+        }
+    });
+};
 
-//grab all elements of a certain type
-const paras = document.querySelectorAll('p');
-paras.forEach(para => {
-    console.log(para);
-});
-console.log(paras);
+// Function to add class
+const addClass = (element, className) => {
+    if (element && !element.classList.contains(className)) {
+        element.classList.add(className);
+    }
+};
 
-//get element by id
-const title = document.getElementById('page-title');
-console.log(title);
+// Function to remove class
+const removeClass = (element, className) => {
+    if (element && element.classList.contains(className)) {
+        element.classList.remove(className);
+    }
+};
 
-//get elements by their class name
-const errors = document.getElementsByClassName('error');
-console.log(errors);
-console.log(errors[0]);
+// Function to toggle class
+const toggleClass = (element, className) => {
+    if (element) {
+        element.classList.toggle(className);
+    }
+};
 
-
-//get elements by tagname
-const paras = document.getElementsByTagName('p');
-console.log(paras);
-console.log(paras[1]);
-//changing text content of an element
-const para = document.querySelector('p');   
-console.log(para.innerText);
-//changing text content of an element
-para.innerText = 'Devs are awesome!';
-//changing text content of an element
-const paras = document.querySelectorAll('p');
-paras.forEach(para => {
-    console.log(para.innerText);
-    para.innerText += ' new text';
-});
-//changing html content of an element
+// Select elements
+const firstPara = document.querySelector('p');
+const allParas = document.querySelectorAll('p');
 const content = document.querySelector('.content');
-console.log(content.innerHTML);
-//changing html content of an element
-content.innerHTML += '<h2>This is a new h2</h2>';
-//changing html content of an element
-const people = ['mario', 'luigi', 'yoshi'];
-people.forEach(person => {
-    content.innerHTML += `<p>${person}</p>`;
-});
-//changing attributes of an element
 const link = document.querySelector('a');
-console.log(link.getAttribute('href')); 
-//changing attributes of an element
-link.setAttribute('href', 'https://www.thenetninja.co.uk');
-//changing attributes of an element
-link.innerText = 'The Net Ninja Website';
-//changing attributes of an element     
-const mssg = document.querySelector('p');
-console.log(mssg.getAttribute('class'));
-//changing attributes of an element
-mssg.setAttribute('class', 'success');
-//changing attributes of an element
-//changing attributes of an element
-mssg.setAttribute('style', 'color: green;');
-//changing attributes of an element
-mssg.style.color = 'red';
-//changing attributes of an element
-mssg.style.margin = '50px';
-//adding classes to an element  
-const content = document.querySelector('p');
-console.log(content.classList);
-//adding classes to an element
-content.classList.add('error');
-//adding classes to an element
-content.classList.remove('error');
-//adding classes to an element
-content.classList.add('success');
-//adding classes to an element
-const paras = document.querySelectorAll('p');
-//adding classes to an element  
-paras.forEach(para => {
-    if(para.textContent.includes('error')){
-        para.classList.add('error');
+const button = document.querySelector('.my-button');
+
+// Manipulate text content of a single paragraph
+if (firstPara) {
+    firstPara.innerText = 'Devs are awesome!';
+}
+
+// Change text content of all paragraphs
+changeParagraphText(allParas, 'Updated text for all paragraphs');
+
+// Update HTML content
+if (content) {
+    content.innerHTML = `
+        <h2>This is a new h2</h2>
+        <p>mario</p>
+        <p>luigi</p>
+        <p>yoshi</p>
+    `;
+}
+
+// Update attributes
+if (link) {
+    console.log(link.getAttribute('href'));
+    link.setAttribute('href', 'https://www.thenetninja.co.uk');
+    link.innerText = 'The Net Ninja Website';
+}
+
+// Add, remove, or toggle classes
+if (firstPara) {
+    addClass(firstPara, 'highlight');
+    removeClass(firstPara, 'error');
+    toggleClass(firstPara, 'success');
+}
+
+// Event listener for button click
+if (button) {
+    button.addEventListener('click', () => {
+        console.log('Button clicked!');
+    });
+}
+
+// Toggle class for paragraphs that contain 'error' or 'success'
+allParas.forEach(para => {
+    if (para.textContent.includes('error')) {
+        addClass(para, 'error');
     }
-    if(para.innerText.includes('success')){
-        para.classList.add('success');
+    if (para.innerText.includes('success')) {
+        addClass(para, 'success');
     }
 });
-//toggle classes
-const title = document.querySelector('.title');
-//toggle classes
-title.classList.toggle('test');
-//toggle classes
-//toggle classes
-title.classList.toggle('test');
-//toggle classes
-//toggle classes
-//parents, children and siblings
-const article = document.querySelector('article');
-console.log(article.children);
-//parents, children and siblings
-
-
